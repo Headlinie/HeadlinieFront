6783d434975dc4ac193070e31f1fd3dcf02ef34e
Victor Bjelkholm <victorbjelkholm@gmail.com>
2015-01-11 15:07:54 +0100

    Use sed instead of patchy filesystem hacks

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #! /bin/sh
 
-#sed -i.bak s/$old_container_port/$new_container_port/ /etc/nginx/sites-enabled/headlinie
+sed -i.bak s/API_PORT_8000_TCP_ADDR/$API_PORT_8000_TCP_ADDR/ /etc/nginx/sites-enabled/api.conf
+sed -i.bak s/API_PORT_8000_TCP_PORT/$API_PORT_8000_TCP_PORT/ /etc/nginx/sites-enabled/api.conf
 
-(echo "upstream api { server $API_PORT_8000_TCP_ADDR:$API_PORT_8000_TCP_PORT; }" && cat /etc/nginx/sites-enabled/api.conf) > api.conf.new
-mv api.conf.new /etc/nginx/sites-enabled/api.conf
-(echo "upstream web { server $WEB_PORT_8000_TCP_ADDR:$WEB_PORT_8000_TCP_PORT; }" && cat /etc/nginx/sites-enabled/web.conf) > web.conf.new
-mv web.conf.new /etc/nginx/sites-enabled/web.conf
+sed -i.bak s/WEB_PORT_8000_TCP_ADDR/$WEB_PORT_8000_TCP_ADDR/ /etc/nginx/sites-enabled/web.conf
+sed -i.bak s/WEB_PORT_8000_TCP_PORT/$WEB_PORT_8000_TCP_PORT/ /etc/nginx/sites-enabled/web.conf
+
+rm /etc/nginx/sites-enabled/web.conf.bak
+rm /etc/nginx/sites-enabled/api.conf.bak
+
 nginx -c /etc/nginx/nginx.conf
